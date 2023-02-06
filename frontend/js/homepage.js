66f264d7cbaa15254f41da77c414b3f51e01ce50
Konstantina Staneva <k.staneva@sap.com>
2023-02-06 23:40:33 +0200

    func and non-func html css js

--- frontend/js/homepage.js
+++ frontend/js/homepage.js
@@ -93,3 +93,24 @@ function fetchRequirementCount(type, element) {
         element.innerHTML = count;
     });
 }
+
+const menuItems = document.querySelectorAll(".list");
+menuItems.forEach((item) => {
+    item.addEventListener('mouseover', (event) => {
+        menuItems.forEach((otherItem) => {
+            if(otherItem.classList.contains("avtive-hover")){
+                otherItem.classList.remove("active-hover");
+            }
+            if(otherItem.firstElementChild.classList.contains("active-hover")) {
+                otherItem.firstElementChild.classList.remove("active-hover");
+            }
+        });
+        item.classList.add("active-hover");
+        item.firstElementChild.classList.add("active-hover");
+    });
+
+    item.addEventListener('mouseout', (event) => {
+        item.classList.remove("active-hover");
+        item.firstElementChild.classList.remove("active-hover");
+    });
+});
