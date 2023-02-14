b8d778a04b172689c00fc0b707595a223b5aade9
Konstantina Staneva <k.staneva@sap.com>
2023-02-14 17:19:01 +0200

    make tables resizable

--- frontend/js/requirements/requirements.js
+++ frontend/js/requirements/requirements.js
@@ -438,3 +438,65 @@ editSaveRequirementButton.addEventListener('click' , (event) => {
     });
 
 })(document);
+
+
+//resizable columns in table functional-requirements table
+let table = document.getElementById('functional-requirements');
+resizableGrid(table);
+
+function resizableGrid(table) {
+    var row = table.getElementsByTagName('tr')[0],
+    cols = row ? row.children : undefined;
+    if (!cols) return;
+
+for (var i=0;i<cols.length;i++){
+    var div = createDiv(table.offsetHeight);
+    cols[i].appendChild(div);
+    cols[i].style.position = 'relative';
+    setListeners(div);
+   }
+}
+
+   function createDiv(height){
+    var div = document.createElement('div');
+    div.style.top = 0;
+    div.style.right = 0;
+    div.style.width = '2px';
+    div.style.position = 'absolute';
+    div.style.cursor = 'col-resize';
+    div.style.backgroundColor = '#eaeaea';
+    div.style.userSelect = 'none';
+    div.style.height = height+'px';
+    return div;
+   }
+
+function setListeners(div){
+    var pageX,curCol,nxtCol,curColWidth,nxtColWidth;
+    div.addEventListener('mousedown', function (e) {
+     curCol = e.target.parentElement;
+     nxtCol = curCol.nextElementSibling;
+     pageX = e.pageX;
+     curColWidth = curCol.offsetWidth
+     if (nxtCol)
+      nxtColWidth = nxtCol.offsetWidth
+    });
+   
+    document.addEventListener('mousemove', function (e) {
+     if (curCol) {
+      var diffX = e.pageX - pageX;
+    
+      if (nxtCol)
+       nxtCol.style.width = (nxtColWidth - (diffX))+'px';
+   
+      curCol.style.width = (curColWidth + diffX)+'px';
+     }
+    });
+   
+   document.addEventListener('mouseup', function (e) { 
+    curCol = undefined;
+    nxtCol = undefined;
+    pageX = undefined;
+    nxtColWidth = undefined;
+    curColWidth = undefined;
+    });
+   }
