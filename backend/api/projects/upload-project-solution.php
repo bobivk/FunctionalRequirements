49b17c89fabdf58c2d5783a2bd9af87747ba3e50
bobivk <bobby.kavalov@gmail.com>
2023-07-04 19:42:51 +0300

    post

--- backend/api/projects/upload-project-solution.php
+++ backend/api/projects/upload-project-solution.php
@@ -6,17 +6,17 @@ use Aws\S3\S3Client;
 
 if(isset($_SESSION["userId"]) && isset($_SESSION["userRoleId"])) {
     $filename = $_FILES["files"]["tmp_name"][0];
-    if(isset($_FILES['files']) && !empty($filename) && $_FILES["files"]["size"] > 0) //{
+    $filesize = $_FILES["files"]["size"];
+    if(isset($_FILES['files']) && !empty($filename) && $filesize > 0) //{
     $s3Client = new S3Client([
         'version' => 'latest',
         'region'  => 'eu-central-1'
     ]);
         $project_id = $_POST["projectId"];
         // Check if file was uploaded without errors
-            // $allowed = array("zip" => "file/zip", "rar" => "file/rar");
+            $allowed = array("zip" => "file/zip", "rar" => "file/rar");
             // $filename = $_FILES["anyfile"]["name"];
             // $filetype = $_FILES["anyfile"]["type"];
-            // $filesize = $_FILES["anyfile"]["size"];
             // Validate file extension
             // $ext = pathinfo($filename, PATHINFO_EXTENSION);
             // if(!array_key_exists($ext, $allowed)){
@@ -24,11 +24,11 @@ if(isset($_SESSION["userId"]) && isset($_SESSION["userRoleId"])) {
             //     echo json_encode(["message" => "Error: Please select a valid file format. Allowed formats: zip, rar"]);
             // }
             // Validate file size - 10MB maximum
-            // $maxsize = 10 * 1024 * 1024;
-            // if($filesize > $maxsize) {
-            //     http_response_code(400);
-            //     echo json_encode(["message" => "Error: Maximum file size is 10 MB"]);
-            // }
+            $maxsize = 10 * 1024 * 1024;
+            if($filesize > $maxsize) {
+                http_response_code(400);
+                echo json_encode(["message" => "Error: Maximum file size is 10 MB"]);
+            }
             // Validate type of the file
             //if(in_array($filetype, $allowed)) {
             // Check whether file exists before uploading it
@@ -38,17 +38,17 @@ if(isset($_SESSION["userId"]) && isset($_SESSION["userRoleId"])) {
                 //     if(move_uploaded_file($_FILES["anyfile"]["tmp_name"], "upload/" . $filename)) {
                         $bucket = 'projects-functional-requirements';
                         //$file_Path = __DIR__ . '/upload/'. $filename;
-                        //try {
-                            // $result = $s3Client->putObject([
-                            //     'Bucket' => $bucket,
-                            //     'Key'    => $project_id,
-                            //     'Body'   => fopen($filename, 'r'),
-                            //     'ACL'    => 'public-read', // make file 'public'
-                            // ]);
+                        try {
+                            $result = $s3Client->putObject([
+                                'Bucket' => $bucket,
+                                'Key'    => $project_id,
+                                'Body'   => fopen($filename, 'r'),
+                                'ACL'    => 'public-read', // make file 'public'
+                            ]);
+                        }
 //                            $file_url_in_s3 = $result->get('ObjectURL');
                 //             // save S3 URL to database
-                http_response_code(200);
-                             echo "File uploaded successfully. File path is: ";//. $file_url_in_s3;
+                             //echo "File uploaded successfully. File path is: ";//. $file_url_in_s3;
                 //             // $db = new DB();
                 //             // $connection = $db->getConnection();
                 //             // $sql = "UPDATE projects SET solution_s3_url=:solution_s3_url where id = :project_id";
