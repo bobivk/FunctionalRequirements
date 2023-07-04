baaee7600d5e2a3d73f9c644d41899cf6c42bc16
bobivk <bobby.kavalov@gmail.com>
2023-07-04 18:39:19 +0300

    comment out

--- backend/api/projects/upload-project-solution.php
+++ backend/api/projects/upload-project-solution.php
@@ -22,49 +22,51 @@ if(isset($_SESSION["userId"]) && isset($_SESSION["userRoleId"])) {
             // Validate file extension
             $ext = pathinfo($filename, PATHINFO_EXTENSION);
             if(!array_key_exists($ext, $allowed)){
-                die("Error: Please select a valid file format. Allowed formats: zip, rar");
+                http_response_code(400);
+                echo json_encode(["message" => "Error: Please select a valid file format. Allowed formats: zip, rar"]);
             }
             // Validate file size - 10MB maximum
             $maxsize = 10 * 1024 * 1024;
             if($filesize > $maxsize) {
-                die("Please upload a file smaller than 10 MB");
+                http_response_code(400);
+                echo json_encode(["message" => "Error: Maximum file size is 10 MB"]);
             }
             // Validate type of the file
             if(in_array($filetype, $allowed)) {
             // Check whether file exists before uploading it
-                if(file_exists("upload/" . $filename)) {
-                    echo $filename . " is already exists.";
-                } else {
-                    if(move_uploaded_file($_FILES["anyfile"]["tmp_name"], "upload/" . $filename)) {
-                        $bucket = 'projects-functional-requirements';
-                        $file_Path = __DIR__ . '/upload/'. $filename;
-                        try {
-                            $result = $s3Client->putObject([
-                                'Bucket' => $bucket,
-                                'Key'    => $project_id,
-                                'Body'   => fopen($file_Path, 'r'),
-                                'ACL'    => 'public-read', // make file 'public'
-                            ]);
-                            $file_url_in_s3 = $result->get('ObjectURL');
-                            // save S3 URL to database
-                            echo "File uploaded successfully. File path is: ". $file_url_in_s3;
-                            $db = new DB();
-                            $connection = $db->getConnection();
-                            $sql = "UPDATE projects SET solution_s3_url=:solution_s3_url where id = :project_id";
-                            $statement = $connection -> prepare($sql);
-                            $statement -> execute(array("solution_s3_url" => $file_url_in_s3, "project_id" => $project_id));
-                            $statement->fetchAll();
-                            http_response_code(200);
-                            echo json_encode(["message" => "File saved successfully"]);
-                        } catch (Aws\S3\Exception\S3Exception $e) {
-                            echo "There was an error uploading the file.\n";
-                            echo $e->getMessage();
-                        }
-                        echo "Your file was uploaded successfully.";
-                    } else {
-                        echo "File is not uploaded";
-                    }
-                } 
+                // if(file_exists("upload/" . $filename)) {
+                //     echo $filename . " is already exists.";
+                // } else {
+                //     if(move_uploaded_file($_FILES["anyfile"]["tmp_name"], "upload/" . $filename)) {
+                //         $bucket = 'projects-functional-requirements';
+                //         $file_Path = __DIR__ . '/upload/'. $filename;
+                //         try {
+                //             $result = $s3Client->putObject([
+                //                 'Bucket' => $bucket,
+                //                 'Key'    => $project_id,
+                //                 'Body'   => fopen($file_Path, 'r'),
+                //                 'ACL'    => 'public-read', // make file 'public'
+                //             ]);
+                //             $file_url_in_s3 = $result->get('ObjectURL');
+                //             // save S3 URL to database
+                //             echo "File uploaded successfully. File path is: ". $file_url_in_s3;
+                //             // $db = new DB();
+                //             // $connection = $db->getConnection();
+                //             // $sql = "UPDATE projects SET solution_s3_url=:solution_s3_url where id = :project_id";
+                //             // $statement = $connection -> prepare($sql);
+                //             // $statement -> execute(array("solution_s3_url" => $file_url_in_s3, "project_id" => $project_id));
+                //             // $statement->fetchAll();
+                //             http_response_code(200);
+                //             echo json_encode(["message" => "File" . $file_url_in_s3 . "saved successfully"]);
+                //         } catch (Aws\S3\Exception\S3Exception $e) {
+                //             echo "There was an error uploading the file.\n";
+                //             echo $e->getMessage();
+                //         }
+                //         echo "Your file was uploaded successfully.";
+                //     } else {
+                //         echo "File is not uploaded";
+                //     }
+                // } 
             } else{
             echo "Error: There was a problem uploading your file. Please try again."; 
             }
