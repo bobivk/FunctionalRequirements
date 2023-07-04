51bcfdc71ddac5b7e9dc8a8783b6da3afe37c847
Konstantina Staneva <k.staneva@sap.com>
2023-07-04 21:25:40 +0300

    modify2

--- backend/api/projects/delete-project-solution.php
+++ backend/api/projects/delete-project-solution.php
@@ -12,7 +12,7 @@ if(isset($_SESSION["userId"]) && isset($_SESSION["userRoleId"])) {
     ]);
     $project_id = $_DELETE["projectId"];
     $get_s3_url_sql = "SELECT solution_s3_url FROM projects where id = :project_id";
-    $get_statement = $connection => prepare($get_s3_url_sql);
+    $get_statement = $connection -> prepare($get_s3_url_sql);
     $get_statement -> execute(array("project_id" => $project_id));
     $solution_s3_url = $get_statement->fetchAll(PDO::FETCH_ASSOC)[0];
 
@@ -24,7 +24,7 @@ if(isset($_SESSION["userId"]) && isset($_SESSION["userRoleId"])) {
     ));
 
     $delete_s3_url_sql = "UPDATE projects SET solution_s3_url = NULL WHERE id = :project_id";
-    $update_statement = $connection => prepare($delete_s3_url_sql);
+    $update_statement = $connection -> prepare($delete_s3_url_sql);
     $get_statement -> execute(array("project_id" => $project_id));
     http_response_code(200);
     echo json_encode(["message" => "Файлът е премахнат успешно."]);
