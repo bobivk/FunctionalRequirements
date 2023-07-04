b5964625f92cf915bcab7e15bb6953c41c9fc192
bobivk <bobby.kavalov@gmail.com>
2023-07-04 21:00:10 +0300

    sada

--- backend/api/projects/upload-project-solution.php
+++ backend/api/projects/upload-project-solution.php
@@ -11,30 +11,25 @@ if(isset($_SESSION["userId"]) && isset($_SESSION["userRoleId"])) {
     $s3Client = new S3Client([
         'version' => 'latest',
         'region'  => 'eu-central-1',
-        // 'credentials' => [
-        //     'key' => 'AKIAQ4TFYYGN3PN3LA22',
-        //     'secret' => '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'
-        // ]
     ]);
         $project_id = $_POST["projectId"];
         // Check if file was uploaded without errors
-            //$allowed = array("zip" => "file/zip", "rar" => "file/rar");
-            // $filename = $_FILES["anyfile"]["name"];
+            $allowed = array("zip" => "file/zip", "rar" => "file/rar");
             // $filetype = $_FILES["anyfile"]["type"];
             // Validate file extension
-            // $ext = pathinfo($filename, PATHINFO_EXTENSION);
-            // if(!array_key_exists($ext, $allowed)){
-            //     http_response_code(400);
-            //     echo json_encode(["message" => "Error: Please select a valid file format. Allowed formats: zip, rar"]);
-            // }
+            $ext = pathinfo($filename, PATHINFO_EXTENSION);
+            if(!array_key_exists($ext, $allowed)){
+                http_response_code(400);
+                echo json_encode(["message" => "Error: Please select a valid file format. Allowed formats: zip, rar"]);
+            }
             // Validate file size - 10MB maximum
             $maxsize = 10 * 1024 * 1024;
-            // if($filesize > $maxsize) {
-            //     http_response_code(400);
-            //     echo json_encode(["message" => "Error: Maximum file size is 10 MB"]);
-            // }
-            // Validate type of the file
-            //if(in_array($filetype, $allowed)) {
+            if($filesize > $maxsize) {
+                http_response_code(400);
+                echo json_encode(["message" => "Error: Maximum file size is 10 MB"]);
+            }
+            //Validate type of the file
+            if(in_array($filetype, $allowed)) {
             // Check whether file exists before uploading it
                 // if(file_exists("upload/" . $filename)) {
                 //     echo $filename . " is already exists.";
@@ -42,15 +37,15 @@ if(isset($_SESSION["userId"]) && isset($_SESSION["userRoleId"])) {
                 //     if(move_uploaded_file($_FILES["anyfile"]["tmp_name"], "upload/" . $filename)) {
                         $bucket = 'projects-functional-requirements';
                         //$file_Path = __DIR__ . '/upload/'. $filename;
-                        try {
-                            $result = $s3Client->putObject([
-                                'Bucket' => $bucket,
-                                'Key'    => $project_id . $filename,
-                                'SourceFile'   => 'get-project.php',
-                                'ACL'    => 'public-read', // make file 'public'
-                            ]);
-                            echo $result;
-                        }
+                        // try {
+                        //     $result = $s3Client->putObject([
+                        //         'Bucket' => $bucket,
+                        //         'Key'    => $project_id . $filename,
+                        //         'SourceFile'   => 'get-project.php',
+                        //         'ACL'    => 'public-read', // make file 'public'
+                        //     ]);
+                        //     echo $result;
+                        // }
 //                            $file_url_in_s3 = $result->get('ObjectURL');
                 //             // save S3 URL to database
                              //echo "File uploaded successfully. File path is: ";//. $file_url_in_s3;
@@ -70,7 +65,7 @@ if(isset($_SESSION["userId"]) && isset($_SESSION["userRoleId"])) {
                 //     } else {
                 //         echo "File is not uploaded";
                 //     }
-                // }
+                }
                 http_response_code(200);
                 echo json_encode(["message" => "great, ". $project_id, " files: " => $_FILES]);
         //     } else {
